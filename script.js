b19234edc433afb44d721854734feeb56031ecc7
onlyjiu0517-netizen <onlyjiu0517@gmail.com>
2026-08-03 10:18:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const jpGroup = document.querySelector(".kana-jp");
+  const enGroup = document.querySelector(".kana-en");
 
+  const toAZ = document.querySelector(".kana-jp .switch-btn");
+  const toJP = document.querySelector(".kana-en .switch-btn");
+
+  toAZ.addEventListener("click", (e) => {
+    e.preventDefault();
+    jpGroup.style.display = "none";
+    enGroup.style.display = "flex";
+  });
+
+  toJP.addEventListener("click", (e) => {
+    e.preventDefault();
+    enGroup.style.display = "none";
+    jpGroup.style.display = "flex";
+  });
+});
